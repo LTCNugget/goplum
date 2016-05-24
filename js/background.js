c7e8474153a04472a1c2d3d5ef5f5b6b89feb8bd
LTCNugget <kylebrott@gmail.com>
2016-05-24 10:59:28 -0400

    fixed issue #3.

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 // background.js
 
-var songData = {  };
+var songData = { "info": {  } };
 var debug = true;
 
 function httpGet( url ) {
@@ -22,19 +22,21 @@ chrome.runtime.onMessage.addListener(function(message) {
   if (message.greeting == "infojson") {
     if (debug) console.log("A message with the greeting of 'infojson' was recieved");
     parsed = JSON.parse(message.text);
-    songData.title = parsed.title;
-    songData.album = parsed.album;
-    songData.artist = parsed.artist;
-    songData.art = parsed.url;
+    songData.info.title = parsed.title;
+    songData.info.album = parsed.album;
+    songData.info.artist = parsed.artist;
+    songData.info.art = parsed.art;
   }
 });
 chrome.webRequest.onCompleted.addListener(function(request) {
   if (debug) console.log("A request matching the filter was completed");
-  chrome.tabs.executeScript({
-    code: 'var songInfo = { }; songInfo.title = $("#currently-playing-title").innerText; songInfo.album = $(".player-album").innerText; songInfo.artist = $(".player-artist").innerText; songInfo.art = $("#playerBarArt").src; console.log(songInfo); chrome.runtime.sendMessage({greeting:"infojson", text:JSON.stringify(songInfo)});'
-  });
-  songData.urls = JSON.parse(httpGet(request.url).replace("\u003d", "=").replace("\u0026", "&")).urls;
   setTimeout(function() {
-    if (songData.artist) httpPost("//24.125.232.31/scripts/goplum.php", JSON.stringify(songData));
+    chrome.tabs.executeScript({
+      code: 'var songInfo = { }; songInfo.title = $("#currently-playing-title").innerText; songInfo.album = $(".player-album").innerText; songInfo.artist = $(".player-artist").innerText; songInfo.art = $("#playerBarArt").src; console.log(songInfo); chrome.runtime.sendMessage({greeting:"infojson", text:JSON.stringify(songInfo)});'
+    });
   }, 2000);
-}, {urls:["https://play.google.com/music/mplay?*"/*,"https://play.google.com/music/wplay?*"*/]});
+  songData.urls = JSON.parse(httpGet(request.url + "&goplum=true").replace("\u003d", "=").replace("\u0026", "&")).urls;
+  setTimeout(function() {
+    if (songData.info.artist) httpPost("//24.125.232.31/scripts/goplum.php", JSON.stringify(songData));
+  }, 2000);
+}, {urls:["https://play.google.com/music/mplay?*&start=0","https://play.google.com/music/wplay?*&start=0"]});
