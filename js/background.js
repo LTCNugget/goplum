6c7cbb9fc968129b7304fe4c9d21e9bd85698c68
LTCNugget <kylebrott@gmail.com>
2016-05-25 20:12:08 -0400

    js/background.js: fixed issue #8, doesn't skip on free radio due to limit, and ignores ads.

--- js/background.js
+++ js/background.js
@@ -11,35 +11,41 @@ function httpGet( url ) {
   return xmlHttp.responseText;
 }
 function httpPost( url, data ) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "POST", url, true );
-    xmlHttp.send( data );
-    if (debug) console.log("A POST request was sent");
-    return xmlHttp.responseText;
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "POST", url, true );
+	xmlHttp.send( data );
+	if (debug) console.log("A POST request was sent");
+	return xmlHttp.responseText;
 }
 
 chrome.runtime.onMessage.addListener(function(message) {
-  if (message.greeting == "infojson") {
-    if (debug) console.log("A message with the greeting of 'infojson' was recieved");
-    parsed = JSON.parse(message.text);
-    songData.info.title = parsed.title;
-    songData.info.album = parsed.album;
-    songData.info.artist = parsed.artist;
-    songData.info.art = parsed.art;
+  if (message.greeting == "goplum-info") {
+		if (debug) console.log("A message with the greeting of 'goplum-info' was recieved");
+	  parsed = JSON.parse(message.text);
+	 	songData.info.title = parsed.title;
+	 	songData.info.album = parsed.album;
+  	songData.info.artist = parsed.artist;
+   	songData.info.art = parsed.art;
+   	songData.status = "song";
+  } else if (message.greeting == "goplum-ad") {
+  	if (debug) console.log("A message with the greeting of 'goplum-ad' was recieved");
+  	songData.status = "advertisement";
   }
 });
 chrome.webRequest.onCompleted.addListener(function(request) {
   if (debug) console.log("A request matching the filter was completed");
-  //setTimeout(function() {
-    //if (debug) console.log("The timeout() for .executeScript has ended");
-    chrome.tabs.executeScript({
-      code: 'var songInfo = { title:document.getElementById("currently-playing-title").innerText, album:document.getElementsByClassName("player-album")[0].innerText, artist:document.getElementsByClassName("player-artist")[0].innerText, art:document.getElementById("playerBarArt").src }; console.log(songInfo); chrome.runtime.sendMessage({greeting:"infojson", text:JSON.stringify(songInfo)});'
-    });
-  //}, 1000);
+	chrome.tabs.executeScript({
+	  code: 'if (document.getElementsByClassName("player-album")[0]!==undefined) {var songInfo={title:document.getElementById("currently-playing-title").innerText,album: document.getElementsByClassName("player-album")[0].innerText,artist:document.getElementsByClassName("player-artist")[0].innerText,art:document.getElementById("playerBarArt").src};chrome.runtime.sendMessage({greeting:"goplum-info",text:JSON.stringify(songInfo)});} else {console.log("advertisement");chrome.runtime.sendMessage({greeting:"goplum-ad"});}'
+	});
   songData.urls = JSON.parse(httpGet(request.url + "&goplum=true").replace("\u003d", "=").replace("\u0026", "&")).urls;
   setTimeout(function() {
-    if (debug) console.log("The timeout() for httpPost() has ended");
-    if (songData.info.artist) httpPost("https://24.125.232.31/scripts/goplum.php", JSON.stringify(songData));
-    setTimeout(chrome.tabs.executeScript({ code:'document.getElementById("player-bar-forward").click()' }), 4000);
+		if (debug) console.log("The timeout for httpPost() has ended");
+		if (songData.status !== "advertisement") {
+			if (debug) console.log("song");
+			httpPost("https://24.125.232.31/scripts/goplum.php", JSON.stringify(songData));
+			if (request.url.substr(30, 5) == "mplay") setTimeout(chrome.tabs.executeScript({ code:'document.getElementById("player-bar-forward").click()' }), 6000);
+		} else {
+			if (debug) console.log("advertisement");
+		}
   }, 1000);
-}, {urls:["https://play.google.com/music/mplay?*&start=0","https://play.google.com/music/wplay?*&start=0"]});
+}, {urls:["https://play.google.com/music/mplay?*&start=0","https://play.google.com/music/wplay?*&start=0"]})
