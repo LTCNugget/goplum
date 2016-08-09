c28475e3fa4e3e614c2fde016780336c3e4fb437
Kyle B <kylebrott@gmail.com>
2016-08-09 13:00:07 -0400

    may have fixed issue #7. added force stop

--- js/background.js
+++ js/background.js
@@ -4,10 +4,16 @@ var settings;
 var songData = { "info": {  } };
 var debug = true;
 var forcestop = false;
-chrome.storage.sync.get("settings", function(getData){console.log(getData); settings=getData.settings});
+chrome.storage.sync.get("settings", function(getData) {
+	console.log(getData);
+	settings = getData.settings
+});
 
 function httpGet( url ) {
-	if (forcestop) return false;
+	if (forcestop) {
+		clearVarsOnStop();
+		return false;
+	}
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.open( "GET", url, false );
 	xmlHttp.send( null );
@@ -15,16 +21,25 @@ function httpGet( url ) {
 	return xmlHttp.responseText;
 }
 function httpPost( url, data ) {
-	if (forcestop) return false;
+	if (forcestop) {
+		clearVarsOnStop();
+		return false;
+	}
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.open( "POST", url, true );
 	xmlHttp.send( data );
 	if (debug) console.log("A POST request was sent");
 	return xmlHttp.responseText;
 }
+function clearVarsOnStop() {
+	songData = null;
+}
 
-chrome.runtime.onMessage.addListener(function(message) {
-chrome.storage.sync.get("settings", function(getData){console.log(getData); settings=getData.settings});
+chrome.runtime.onMessage.addListener( function(message) {
+	chrome.storage.sync.get("settings", function(getData) {
+		console.log(getData);
+		settings = getData.settings
+	});
 	if (message.greeting == "goplum-info") {
 		if (debug) console.log("A message with the greeting of 'goplum-info' was recieved");
 		parsed = JSON.parse(message.text);
@@ -34,28 +49,47 @@ chrome.storage.sync.get("settings", function(getData){console.log(getData); sett
 	   	songData.info.art = parsed.art;
 	   	songData.status = "song";
 	} else if (message.greeting == "goplum-ad") {
-  	if (debug) console.log("A message with the greeting of 'goplum-ad' was recieved");
-  	songData.status = "advertisement";
-  }
+		if (debug) console.log("A message with the greeting of 'goplum-ad' was recieved");
+		songData.status = "advertisement";
+	}
 });
-chrome.webRequest.onCompleted.addListener(function(request) {
+chrome.webRequest.onCompleted.addListener( function(request) {
 	if (debug) console.log("A request matching the filter was completed");
 	if (!forcestop) {
 		chrome.tabs.executeScript({
-			code: 'if (document.getElementsByClassName("player-album")[0]!==undefined) {var songInfo={title:document.getElementById("currently-playing-title").innerText,album: document.getElementsByClassName("player-album")[0].innerText,artist:document.getElementsByClassName("player-artist")[0].innerText,art:document.getElementById("playerBarArt").src};chrome.runtime.sendMessage({greeting:"goplum-info",text:JSON.stringify(songInfo)});} else {console.log("advertisement");chrome.runtime.sendMessage({greeting:"goplum-ad"});}'
+			file: inject.js
 		});
+	} else {
+		clearVarsOnStop();
 	}
 	songData.urls = JSON.parse(httpGet(request.url + "&goplum=true").replace("\u003d", "=").replace("\u0026", "&")).urls;
-	setTimeout(function() {
+	setTimeout( function() {
 		if (debug) console.log("The timeout for httpPost() has ended");
 		if (songData.status !== "advertisement") {
 			if (debug) console.log(songData);
 			console.log(settings.postsite);
-			if (!forcestop) {httpPost(settings.postsite, JSON.stringify(songData));}
-			
-			if (request.url.substr(30, 5) == "mplay" && !forcestop) setTimeout(function() { chrome.tabs.executeScript({ code:'document.getElementById("player-bar-forward").click()' }) }, 5000);
+			if (!forcestop) {
+				httpPost(settings.postsite, JSON.stringify(songData));
+			} else {
+				clearVarsOnStop();
+			}
+			if (request.url.substr(30, 5) == "mplay" && !forcestop) {
+				setTimeout( function() {
+					if (!forcestop) {
+						chrome.tabs.executeScript({
+							code:'document.getElementById("player-bar-forward").click()'
+						}) }, 5000);
+					} else {
+						clearVarsOnStop();
+					}
+				}
 		} else {
 			if (debug) console.log("advertisement");
 		}
-  }, 1000);
-}, {urls:["https://play.google.com/music/mplay?*&start=0","https://play.google.com/music/wplay?*&start=0"]});
+	}, 1000);
+}, {
+	urls: [
+		"https://play.google.com/music/mplay?*&start=0",
+		"https://play.google.com/music/wplay?*&start=0"
+	]
+});
