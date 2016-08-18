328f8bcf9249672982a1afdab75a4c16d854a4f3
Kyle B <kylebrott@gmail.com>
2016-08-18 10:31:40 -0400

    added more player controls and moved to seperate file.

--- js/inject.js
+++ js/inject.js
@@ -1,19 +1,20 @@
+var debug = true;
 if (document.getElementsByClassName("player-album")[0] !== undefined) {
-	console.log("song");
+	if (debug) console.log("song");
 	var songInfo = {
 		title: document.getElementById("currently-playing-title").innerText,
 		album: document.getElementsByClassName("player-album")[0].innerText,
 		artist: document.getElementsByClassName("player-artist")[0].innerText,
 		art: document.getElementById("playerBarArt").src
 	};
-	console.log(songInfo);
+	if (debug) console.log(songInfo);
 	chrome.runtime.sendMessage({
 		greeting: "goplum-info",
 		text: JSON.stringify(songInfo)
 	});
-	console.log("finished sending info");
+	if (debug) console.log("finished sending info");
 } else {
-	console.log("advertisement");
+	if (debug) console.log("advertisement");
 	chrome.runtime.sendMessage({
 		greeting: "goplum-ad"
 	});
